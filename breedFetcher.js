befed9f8fd7509b3983daccbd78584094acc15c8
d33zhou <d33zhou@outlook.com>
2021-12-02 19:24:42 +0000

    refactor code to split module and function, and add M&C tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,29 @@
 const request = require('request');
-const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+/* request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+ 
   if (error) return console.log("Error: Request failed.", error);
 
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log(`Breed '${breed}' was not found`);
-  } else {
-    console.log(data[0].description);
-  }
-});
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log(`Breed '${breed}' was not found`);
+    } else {
+      console.log(data[0].description);
+    }
+  }); */
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    let data;
+
+    !error ? data = JSON.parse(body) : "";
+
+    if (error || data.length === 0) {
+      callback(`Error - please try again. ${error ? error : 'No breed found.'}`, null);
+    } else {
+      callback(error, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
